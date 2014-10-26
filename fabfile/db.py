ce43bb877caa340203e5ca83be00f6212d165f6f
nmizoguc <nmizoguc@yahoo-corp.jp>
2014-10-26 23:09:57 +0900

    fixed bug in db.py

--- fabfile/db.py
+++ fabfile/db.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from common import Component
+from common import Component, get_hosts
 from fabric.api import run, execute
 from functools import partial
 
@@ -17,10 +17,10 @@ def backup():
     run('echo "Backup database"')
 
 class Database(Component):
-    def service_out(self, hosts):
+    def service_out(self, *hosts):
         for host in hosts:
             execute(partial(service_out, host), hosts=get_hosts('ap'))
-    def service_in(self, hosts):
+    def service_in(self, *hosts):
         for host in hosts:
             execute(partial(service_in, host), hosts=get_hosts('ap'))
     def restart(self, hosts):
